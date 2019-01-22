4af5aaac607a7643c96cf0da38af44e149a08eb6
Will Saults <wsaults@yahoo.com>
2019-01-21 20:52:43 -0600

    UserRepo code cleanup

--- src/main/java/com/wsaults/budget/UserRepository.java
+++ src/main/java/com/wsaults/budget/UserRepository.java
@@ -6,13 +6,4 @@ import java.util.List;
 public interface UserRepository extends CrudRepository<User, Long> {
 
   List<User> findByName(String name);
-  // User find(Long id);
-
-  // User create (User id);
-
-  // List<User> list();
-
-  // User update(Long id, User user);
-
-  // void delete(Long id);
 }
