54202f5ec1e5a4dadfe18565f61b17678bde500f
Will Saults <wsaults@yahoo.com>
2019-02-01 19:47:51 -0600

    Add lombok and removed getter and setters in categories. Removed iconName in categories

--- src/main/java/com/wsaults/budget/Category.java
+++ src/main/java/com/wsaults/budget/Category.java
@@ -5,7 +5,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Data;
+
 @Entity
+@Data
 public class Category {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
@@ -17,50 +20,11 @@ public class Category {
 
   private Float max;
 
-  private String iconName;
-
   protected Category() {}
 
-  public Category(String name, Float amount, Float max, String iconName) {
+  public Category(String name, Float amount, Float max) {
     this.name = name;
     this.amount = amount;
     this.max = max;
-    this.iconName = iconName;
-  }
-
-  public long getId() {
-    return this.id;
-  }
-
-  public String getName() {
-    return this.name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public Float getAmount() {
-    return this.amount;
-  }
-
-  public void setAmount(Float amount) {
-    this.amount = amount;
-  }
-
-  public Float getMax() {
-    return this.max;
-  }
-
-  public void setMax(Float max) {
-    this.max = max;
-  }
-  
-  public String getIconName() {
-    return this.iconName;
-  }
-
-  public void setIconName(String iconName) {
-    this.iconName = iconName;
   }
 }
