a5ecad36678c43558b197f3429b30487b3d24570
Will Saults <wsaults@yahoo.com>
2019-02-05 16:08:25 -0600

    set star for all cors filters

--- src/main/java/com/wsaults/budget/RestConfig.java
+++ src/main/java/com/wsaults/budget/RestConfig.java
@@ -7,6 +7,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import java.util.*;
 
 @Configuration
 public class RestConfig extends WebSecurityConfigurerAdapter {
@@ -16,6 +17,7 @@ public class RestConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
       http.cors();
+      http.csrf().disable();
   }
 
   public RestConfig(CorsConfigurationProperties corsConfigurationProperties) {
@@ -26,11 +28,18 @@ public class RestConfig extends WebSecurityConfigurerAdapter {
   public CorsFilter corsFilter() {
       UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
       CorsConfiguration config = new CorsConfiguration();
-      config.setAllowedOrigins(corsConfigurationProperties.getDomains());
-      config.setAllowedHeaders(corsConfigurationProperties.getHeaders());
-      config.setAllowedMethods(corsConfigurationProperties.getMethods());
-      config.setAllowCredentials(true);
+      
+      // config.setAllowedHeaders(corsConfigurationProperties.getHeaders());
+      // List<String> domains = new ArrayList<>(Arrays.asList("*"));
+      List<String> headers = new ArrayList<>(Arrays.asList("Origin", "Accept", "X-Requested-With", "Content-Type", "Access-Control-Request-Methods", "Access-Control-Request-Headers", "Authorization", "Access-Control-Allow-Origin"));
+      List<String> methods = new ArrayList<>(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+      
+      // config.setAllowedOrigins(domains);
+      // config.setAllowedHeaders(headers);
+      // config.setAllowedMethods(methods);
       config.addAllowedOrigin("*");
+      config.addAllowedHeader("*");
+      config.addAllowedMethod("*");
       source.registerCorsConfiguration("/**", config);
       return new CorsFilter(source);
   }
